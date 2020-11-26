74da985319af80010995dc77abff297d6caa9653
cyzhakt7 <wykim970@gmail.com>
2020-11-26 16:58:50 +0900

    Back End Server and User Schema

---
+++ schema/User.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+const userSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    token: String,
+    
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    
+    password: {
+        type: String,
+        minlength: 4
+    },
+   
+    role: {
+        type: Number,
+        default: 0      // 0 : 학생     1 : 강의자
+    }
+})
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+     // 패스워드 암호화  
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else next();
+})
+
+// 패스워드 비교
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+// 토큰 생성
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token
+    user.save(function(err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    })
+}
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const server = express();
+const port = 4000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require("./schema/User");
+
+// 데이터베이스 연결
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://dbUser:9282@mycluster.1og6o.mongodb.net/pClassDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+server.use(cookieParser());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true}));
+
+// 회원가입
+server.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({ success: true })
+    })
+})
+
+// 로그인
+server.post('/api/users/login', (req, res) => {
+    
+    // 요청된 이메일 탐색
+    User.findOne({ email: req.body.email }, (err,user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "존재하지 않는 이메일입니다."
+            })
+        }
+       
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다"});
+
+             user.generateToken((err, user) => {
+                 if (err) return res.status(400).send(err);
+                    
+                 res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+             })   
+        })
+    })
+})
+
+// 터미널에 포트 번호 출력
+server.listen(port, () => console.log(`Listening on port ${port}`));
